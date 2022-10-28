bb6938c7482e37b0d8a42f1a2de98e9c8065e475
AlinderS <simon.alinder@astro.lu.se>
2022-10-28 16:41:56 +0200

    Updates all files to use `black` format

--- 2-numpy/lecture_functions.py
+++ 2-numpy/lecture_functions.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fracformat(x,pos):
-    frac = Fraction(x/np.pi)
+
+def fracformat(x, pos):
+    frac = Fraction(x / np.pi)
     if frac.numerator == 0:
         return 0
     elif frac.numerator == frac.denominator:
@@ -12,34 +13,39 @@ def fracformat(x,pos):
     elif frac.denominator == 1:
         return r"${:2d} \pi$".format(frac.numerator)
     else:
-        return r"$\frac{{ {:2d} \pi }}{{ {:2d} }}$".format(frac.numerator,frac.denominator)
+        return r"$\frac{{ {:2d} \pi }}{{ {:2d} }}$".format(
+            frac.numerator, frac.denominator
+        )
 
 
 def cart2polar(arr):
-    arr = np.array((np.linalg.norm(arr,axis=1),np.arctan2(arr[:,1], arr[:,0]))).T
+    arr = np.array((np.linalg.norm(arr, axis=1), np.arctan2(arr[:, 1], arr[:, 0]))).T
     return arr
 
+
 def cart2spherical(arr):
-    R = np.linalg.norm(arr,axis=1)
-    theta = np.arctan2(np.linalg.norm(arr[:,:2],axis=1), arr[:,2])
-    phi = np.arctan2(arr[:,1], arr[:,0])
+    R = np.linalg.norm(arr, axis=1)
+    theta = np.arctan2(np.linalg.norm(arr[:, :2], axis=1), arr[:, 2])
+    phi = np.arctan2(arr[:, 1], arr[:, 0])
     arr = np.array((R, theta, phi)).T
     return arr
 
+
 def polynomial_xy(npoints):
-    x = np.linspace(-5,5,npoints)
-    y = (2*x**2 - 2*x + 40) + (10*np.random.normal(size=x.shape))
+    x = np.linspace(-5, 5, npoints)
+    y = (2 * x**2 - 2 * x + 40) + (10 * np.random.normal(size=x.shape))
     return x, y
 
+
 def plot_polynomial(x, y, a=None, b=None, c=None):
-    plt.figure(figsize=(4,2))
-    plt.plot(x,y,color='b', marker='o', linewidth=0, alpha=0.5)
+    plt.figure(figsize=(4, 2))
+    plt.plot(x, y, color="b", marker="o", linewidth=0, alpha=0.5)
 
-    if all([args != None for args in (a,b,c)]):
-        plt.plot(x, a*x**2 + b*x + c,'r')
+    if all([args != None for args in (a, b, c)]):
+        plt.plot(x, a * x**2 + b * x + c, "r")
 
-    plt.ylabel('$y$', fontsize=14)
-    plt.xlabel('$x$', fontsize=14)
+    plt.ylabel("$y$", fontsize=14)
+    plt.xlabel("$x$", fontsize=14)
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
     plt.plot()
